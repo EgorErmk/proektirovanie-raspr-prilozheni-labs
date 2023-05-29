cc2394accd04b2b717ed7cbdb3ce50e20b8a805e
ErmkEgor <ermk.egor@yandex.ru>
2023-05-29 14:55:00 +0400

    first lab

---
+++ lab1/server.py
@@ -0,0 +1,14 @@
+import socket
+import matplotlib.pyplot as plt
+import pickle
+if __name__ == '__main__':
+    server_socket = socket.socket()
+    server_socket.bind(('', 1337))
+    hello_there = plt.imread('images/general_kenobi.jpg')
+    serialized = pickle.dumps(hello_there, protocol=0)
+    server_socket.listen(1)
+    connection, address = server_socket.accept()
+    print(f'client {connection} {address} connected')
+
+    connection.send(serialized)
+    print('Data sent.')
